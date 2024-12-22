9c0f1fa7181cd0b8285b89462821def509a12989
balafutianos <tasosanas2002@gmail.com>
2024-12-22 16:17:34 +0200

    j

---
+++ livespace/src/Frontend/Signup.jsx
@@ -0,0 +1,26 @@
+// Home.jsx
+import React from "react";
+// import Navbar from "./Navbar";
+// import "./menu.css";
+
+function Signup() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <div className="container">
+        <h1>
+          Website
+          <br />
+          Coming Soon!
+        </h1>
+
+        <h1>
+          <strong>Paxyn </strong> 
+        </h1>
+       
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
